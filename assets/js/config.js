3e42e9bc800c915d104f60e3e91d9a1f80ee7719
Eli <eli@thebyrdnest.net>
2022-02-10 16:05:56 -0600

    Initial Commit

---
+++ assets/js/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAyF_X3QjUDyL7srf95MC3nIa2tNnMpaxY",
+    authDomain: "missouri-sandt-class-list.firebaseapp.com",
+    projectId: "missouri-sandt-class-list",
+    storageBucket: "missouri-sandt-class-list.appspot.com",
+    messagingSenderId: "920916963210",
+    appId: "1:920916963210:web:4dc93fab242f54654fe273",
+    measurementId: "G-XY80X3DEJ2",
+    databaseURL: "https://troop59-e264b-default-rtdb.firebaseio.com/"
+};
+
+export {firebaseConfig};
